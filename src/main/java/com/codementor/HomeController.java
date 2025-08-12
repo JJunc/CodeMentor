588c21bd14a160c9491d1719032580ea19f6c29f
JJunc <y75588@gmail.com>
2025-08-12 23:22:54 +0900

    refactor: 레이아웃, 메인페이지 수정

--- src/main/java/com/codementor/HomeController.java
+++ src/main/java/com/codementor/HomeController.java
@@ -1,13 +1,26 @@
 package com.codementor;
 
+import com.codementor.post.enums.PostCategory;
+import com.codementor.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final PostService postService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("freePosts", postService.getPostsByCategory(PostCategory.FREE, PageRequest.of(0, 5)));
+        model.addAttribute("backendPosts", postService.getPostsByCategory(PostCategory.BACKEND, PageRequest.of(0, 5)));
+        model.addAttribute("frontendPosts", postService.getPostsByCategory(PostCategory.FRONTEND, PageRequest.of(0, 5)));
+        model.addAttribute("jobPosts", postService.getPostsByCategory(PostCategory.JOB, PageRequest.of(0, 5)));
+
         return "main";
     }
 }
